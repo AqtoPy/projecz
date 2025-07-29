db48f7fac33649a6ba25be43d52c9afa30d0612d
AqtoPy <rekikreki66@gmail.com>
2025-07-29 18:10:51 +0300

    Create GameOverScreen.gd

---
+++ GameOverScreen.gd
@@ -0,0 +1,9 @@
+extends CanvasLayer
+
+func _on_retry_pressed():
+	get_tree().paused = false
+	get_tree().reload_current_scene()
+
+func _on_main_menu_pressed():
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://scenes/ui/MainMenu.tscn")
