12442c68efa012d634561fbcc4924e03f540502d
AqtoPy <rekikreki66@gmail.com>
2025-07-29 18:57:16 +0300

    Update bullet.gd

--- bullet.gd
+++ bullet.gd
@@ -1,22 +1,27 @@
 extends Area2D
 
 # Настройки
-var speed = 500
-var direction = Vector2.UP  # Летит вверх по умолчанию
-var damage = 25
+@export var speed: float = 800          # Скорость пули
+@export var damage: int = 25            # Урон по ракетам
+@export var max_distance: float = 1200  # Макс. дистанция перед исчезновением
+
+var direction: Vector2 = Vector2.UP     # Направление (задается пушкой)
+var distance_traveled: float = 0        # Пройденная дистанция
 
 func _ready():
-	# Автоматически удаляется, если вылетает за экран
-	set_physics_process(true)
+    # Поворачиваем спрайт по направлению движения
+    rotation = direction.angle() + PI/2
 
 func _physics_process(delta):
-	position += direction * speed * delta
-
-	# Удаление при выходе за границы экрана
-	if position.y < -50 or position.y > get_viewport_rect().size.y + 50:
-		queue_free()
+    var movement = direction * speed * delta
+    position += movement
+    distance_traveled += movement.length()
+    
+    # Удаление при выходе за пределы дистанции
+    if distance_traveled >= max_distance:
+        queue_free()
 
 func _on_body_entered(body):
-	if body.is_in_group("missiles"):  # Если попала в ракету
-		body.take_damage(damage)
-		queue_free()
+    if body.is_in_group("missiles"):
+        body.take_damage(damage)
+        queue_free()  # Уничтожаем пулю при попадании
