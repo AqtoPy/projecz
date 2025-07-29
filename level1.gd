d90875394ac12a97bc40ab0e13e98cdf5c52c711
AqtoPy <rekikreki66@gmail.com>
2025-07-29 18:59:15 +0300

    Update level1.gd

--- level1.gd
+++ level1.gd
@@ -1,33 +1,66 @@
 extends Node2D
 
-@export var missile_scenes: Array[PackedScene] = [
-	preload("res://scenes/missiles/StandardMissile.tscn"),
-	preload("res://scenes/missiles/ZigZagMissile.tscn")
+# Настройки волн
+@export var waves: Array[Dictionary] = [
+    {
+        "delay": 2.0,
+        "missiles": [
+            {"type": "STANDARD", "count": 5},
+            {"type": "ZIGZAG", "count": 2}
+        ]
+    },
+    {
+        "delay": 3.0,
+        "missiles": [
+            {"type": "HOMING", "count": 3},
+            {"type": "BOMBER", "count": 1}
+        ]
+    }
 ]
-@export var missile_count: int = 10
-@export var spawn_interval: float = 2.0
 
-var missiles_spawned: int = 0
+var current_wave: int = 0
 @onready var spawn_timer: Timer = $SpawnTimer
 
 func _ready():
-	spawn_timer.wait_time = spawn_interval
-	spawn_timer.start()
+    start_next_wave()
+
+func start_next_wave():
+    if current_wave >= waves.size():
+        victory()
+        return
+    
+    var wave = waves[current_wave]
+    spawn_timer.wait_time = wave["delay"]
+    spawn_timer.start()
+    
+    for missile_data in wave["missiles"]:
+        for i in range(missile_data["count"]):
+            spawn_missile(missile_data["type"])
+    
+    current_wave += 1
+
+func spawn_missile(type: String):
+    var missile = preload("res://scenes/Missile.tscn").instantiate()
+    
+    # Установка типа через enum
+    match type:
+        "STANDARD":
+            missile.type = MissileType.STANDARD
+        "ZIGZAG":
+            missile.type = MissileType.ZIGZAG
+        "HOMING":
+            missile.type = MissileType.HOMING
+        "BOMBER":
+            missile.type = MissileType.BOMBER
+    
+    missile.position = Vector2(
+        randf_range(50, get_viewport_rect().size.x - 50),
+        -50
+    )
+    add_child(missile)
 
 func _on_spawn_timer_timeout():
-	if missiles_spawned >= missile_count:
-		spawn_timer.stop()
-		return
-	
-	var missile: Area2D
-	
-	# Выбираем случайный тип ракеты
-	if randf() < 0.7:  # 70% шанс обычной ракеты
-		missile = missile_scenes[0].instantiate()
-	else:
-		missile = missile_scenes[1].instantiate()
-	
-	# Случайная позиция сверху экрана
-	missile.position = Vector2(randf_range(50, get_viewport_rect().size.x - 50), -20)
-	add_child(missile)
-	missiles_spawned += 1
+    start_next_wave()
+
+func victory():
+    get_tree().change_scene_to_file("res://scenes/VictoryScreen.tscn")
